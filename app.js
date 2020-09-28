99ec5ccd42b923deaf42d2ee3bf89143e0d09221
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-28 12:15:11 +0530

    Get Coordinates by Mapbox API

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const axios = require('axios');
 
+const getCoordinates = async () => {
+    const response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/Bhayandar.json?access_token=pk.eyJ1IjoiaWFtcmFodWxwYW5jaGFsIiwiYSI6ImNrZm01ajExNzI5aTAycW8zaHdzcDhsdW0ifQ.lC085wAf1ccyP7RvRfP8CQ`);
+    return response.data.features[0].geometry.coordinates;
+}
+
 const getWeather = async () => {
-    const response = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=19.076090&lon=72.877426&key=74e3a1e94efd425296657841f75a852f`);
+    const coordResponse = await getCoordinates();
+    const lat = coordResponse[1];
+    const lon = coordResponse[0];
+    const response = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=74e3a1e94efd425296657841f75a852f`);
     return response.data.data;
 }
 
