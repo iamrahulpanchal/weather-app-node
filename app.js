76d3b3a3dacb210bb23e76256bac9409f12e047f
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-28 13:44:02 +0530

    Getting Place Name from Mapbox API

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const axios = require('axios');
 
-const getCoordinates = async () => {
-    const response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/Bhayandar.json?access_token=pk.eyJ1IjoiaWFtcmFodWxwYW5jaGFsIiwiYSI6ImNrZm01ajExNzI5aTAycW8zaHdzcDhsdW0ifQ.lC085wAf1ccyP7RvRfP8CQ&limit=1`);
-    return response.data.features[0].geometry.coordinates;
+const getCoordinates = async (loc) => {
+    // Suppose if there is ? in our search text, then it will become %3F. If we dont use encodeURIComponent, then the program would crash.
+    loc = encodeURIComponent(loc);
+    const response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${loc}.json?access_token=pk.eyJ1IjoiaWFtcmFodWxwYW5jaGFsIiwiYSI6ImNrZm01ajExNzI5aTAycW8zaHdzcDhsdW0ifQ.lC085wAf1ccyP7RvRfP8CQ&limit=1`);
+    return {
+        latitude : response.data.features[0].center[1],
+        longitude : response.data.features[0].center[0],
+        location : response.data.features[0].place_name,
+    }
 }
 
-const getWeather = async () => {
-    const coordResponse = await getCoordinates();
-    const lat = coordResponse[1];
-    const lon = coordResponse[0];
+const getWeather = async (loc) => {
+    const coordResponse = await getCoordinates(loc);
+    const lat = coordResponse.latitude;
+    const lon = coordResponse.longitude;
+    const location = coordResponse.location;
     const response = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=74e3a1e94efd425296657841f75a852f`);
-    return response.data.data;
+    return {
+        data: response.data.data,
+        location: location
+    }
 }
 
-getWeather().then((resp) => {
-    console.log(`Temperature is ${resp[0].temp}`);
+getWeather('Bhayandar').then((resp) => {
+    // console.log(`Temperature is ${resp[0].temp}`);
+    console.log(`Temperature : ${resp.data[0].temp}`);
+    console.log(`Location : ${resp.location}`);
 }).catch((err) => {
-    console.log(`Unable to Connect to Weather Service. ${err}`);
+    console.log(`Unable to Fetch Weather Data. ${err}`);
 });
