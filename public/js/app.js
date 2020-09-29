fd61d42406df3765999ac8a0b74e66a159ecd241
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-29 18:24:23 +0530

    Adding Catch in Fetch API

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
-// const getData = async () => {
-//     const response = await axios.get(`http://localhost:3000/weather?address=bhy`);
-//     console.log(response.data);
-// }
-
 fetch(`http://localhost:3000/weather?address=bhy`).then((resp) => {
     resp.json().then((data) => {
         console.log(data);
-    })
+    }).catch((err) => {
+        console.log(`Cant Fetch Data`);
+    });
 })
 
