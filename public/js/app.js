65d53ed8c7aa4b719248abf6ee5a90218b3ee1a9
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-29 18:37:58 +0530

    Fetching Data with User Input Location

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,19 @@
-fetch(`http://localhost:3000/weather?address=bhy`).then((resp) => {
-    resp.json().then((data) => {
-        console.log(data);
-    }).catch((err) => {
-        console.log(`Cant Fetch Data`);
+const weatherForm = document.querySelector('form');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const search = document.getElementById('search_text').value;
+    fetch(`http://localhost:3000/weather?address=${search}`).then((resp) => {
+        resp.json().then((data) => {
+            console.log(data);
+        }).catch((err) => {
+            console.log(`Cant Fetch Data`);
+        });
     });
 })
+
+
+
+
 
