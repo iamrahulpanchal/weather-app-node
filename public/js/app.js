d9e805f3421d2c37a045bd3f526a9094c8c73f25
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-29 18:48:26 +0530

    Adding Elements to Display Forecast

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 const weatherForm = document.querySelector('form');
-
+const tempEl = document.getElementById('temp');
+const forecastEl = document.getElementById('forecast');
+const locationEl = document.getElementById('location');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -7,6 +9,9 @@ weatherForm.addEventListener('submit', (e) => {
     fetch(`http://localhost:3000/weather?address=${search}`).then((resp) => {
         resp.json().then((data) => {
             console.log(data);
+            // tempEl.value = data.temperature;
+            // forecastEl.value = data.forecast;
+            // locationEl.value = data.location;
         }).catch((err) => {
             console.log(`Cant Fetch Data`);
         });
